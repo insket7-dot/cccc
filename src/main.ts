d6daa86285fe38e516dd32c629f215ff73f822ef
Qinguo.Ji <qingguo.ji@rydeen.com.cn>
2025-10-14 14:53:00 +0800

    feat: 配置文件修改

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+console.time('total-load');
+console.time('reflect-metadata');
 import 'reflect-metadata';
+console.timeEnd('reflect-metadata');
+console.timeEnd('total-load');
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { appConfig } from '@app/app.config';
+import { App } from '@app/app';
 import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader';
 
 console.log('Starting application initialization...');
