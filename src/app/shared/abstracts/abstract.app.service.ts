1e890431666efcff5a7700f51434331fb505930f
Qinguo.Ji <qingguo.ji@rydeen.com.cn>
2025-10-22 17:28:53 +0800

    feat: 优化http取值device id

--- src/app/shared/abstracts/abstract.app.service.ts
+++ src/app/shared/abstracts/abstract.app.service.ts
@@ -6,31 +6,37 @@ import {
     ResultVO,
     Url,
 } from '@rydeen/angular-framework';
-import { inject, signal, effect } from '@angular/core';
+import { inject, signal } from '@angular/core';
 import { AppEvent } from '@app/core/constants/app.event';
 import { CacheKey } from '@app/shared/constants/cache.key';
 
 export abstract class AbstractAppService extends AbstractService {
     private readonly eventManager = inject(EventManager);
     private deviceId = signal<string>('no_device_id');
+    private readonly initializationPromise: Promise<void> | null = null;
 
     constructor() {
         super();
+        this.initializationPromise = this.loadDeviceId();
+    }
 
-        effect(async () => {
-            const id = await this.getLocalDeviceId();
-            console.log('request read device id', id);
+    private async loadDeviceId(): Promise<void> {
+        try {
+            const id: string | null = await LocalStorage.getItem(CacheKey.DEVICE_ID);
+            console.log('初始化设备ID:', id);
             if (id) {
                 this.deviceId.set(id);
             }
-        });
-    }
-
-    private async getLocalDeviceId(): Promise<string | null> {
-        return await LocalStorage.getItem(CacheKey.DEVICE_ID);
+        } catch (error) {
+            console.error('读取设备ID失败:', error);
+            this.deviceId.set('no_device_id');
+        }
     }
 
     override async request<T>(url: Url, body?: any, header?: RequestHeader): Promise<ResultVO<T>> {
+        if (!this.initializationPromise) {
+            console.error('请先初始化设备ID');
+        }
         if (!['HEART_BEAT'].includes(url.name)) {
             this.eventManager.publish(AppEvent.SHOW_GLOBAL_LOADING, true);
         }
